0eea6cb2d640c7e0b63bd11278de4300641e4963
hhe44 <hhe@mailfence.com>
2024-11-12 00:53:12 -0500

    set theme colors for app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1e40af",
+          secondary: "#facc15",
+          accent: "#ffffff",
+          neutral: "#ff00ff",
+          "base-100": "#1f2937",
+          info: "#3b82f6",
+          success: "#22c55e",
+          warning: "#facc15",
+          error: "#dc2626",
+        },
+      },
+    ],
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
-}
+  plugins: [require("daisyui")],
+};
