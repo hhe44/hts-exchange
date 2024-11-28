85b41a605bf4f6dacf0964ec82084b36504f0553
hhe44 <hhe@mailfence.com>
2024-11-28 13:50:04 -0500

    remove hh/mm/ss options from formatTimeStamp

--- src/components/OptionsTable.tsx
+++ src/components/OptionsTable.tsx
@@ -8,9 +8,6 @@ const formatTimestamp = (utcTimestamp: number): string => {
   // Convert seconds to milliseconds
   const date = new Date(utcTimestamp * 1000);
   const options: Intl.DateTimeFormatOptions = {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
     month: "2-digit",
     day: "2-digit",
     year: "2-digit",
